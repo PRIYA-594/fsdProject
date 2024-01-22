b969e09f3cc1656e1c7fada3eb1449599859a10f
PRIYA-594 <97585592+PRIYA-594@users.noreply.github.com>
2024-01-22 23:00:38 +0530

    Add files via upload

---
+++ show.js
@@ -0,0 +1,45 @@
+function showMore() {
+
+    var listData = Array.prototype.slice.call(document.querySelectorAll('#dataLi li:not(.shown)')).slice(0, 3);
+
+  for (var i=0; i < listData.length; i++)
+  {
+    listData[i].className  = 'shown';
+  }
+  switchButtons();
+}
+
+function showLess() {
+    var listData = Array.prototype.slice.call(document.querySelectorAll('#dataLi li:not(.hidden)')).slice(-3);
+  for (var i=0; i < listData.length; i++)
+  {
+    listData[i].className  = 'hidden';
+  }
+  switchButtons();
+}
+
+function switchButtons() {
+    var hiddenElements = Array.prototype.slice.call(document.querySelectorAll('#dataLi li:not(.shown)'));
+  if(hiddenElements.length == 0)
+  {
+    document.getElementById('moreButton').style.display = 'none';
+  }
+  else
+  {
+    document.getElementById('moreButton').style.display = 'block';
+  }
+
+  var shownElements = Array.prototype.slice.call(document.querySelectorAll('#dataLi li:not(.hidden)'));
+  if(shownElements.length == 0)
+  {
+    document.getElementById('lessButton').style.display = 'none';
+  }
+  else
+  {
+    document.getElementById('lessButton').style.display = 'block';
+  }
+}
+
+onload= function(){
+    showMore();
+}
